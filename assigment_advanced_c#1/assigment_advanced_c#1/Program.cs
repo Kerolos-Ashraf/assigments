e9e23b5ef6be5341202e1a183b2a3e72ce9de2fe
Kerolos <ashrfkyrls49@gmail.com>
2025-08-05 19:00:24 +0300

    add assigment_advanced_c#1

---
+++ assigment_advanced_c#1/assigment_advanced_c#1/Program.cs
@@ -0,0 +1,92 @@
+﻿namespace assigment_advanced_c_1
+{
+    internal class Program
+    {
+
+        #region Q1
+        static void OptimizeBubbleSort(int[] arr)
+        {
+            bool swapped ;
+
+            for (int i = 0; i < arr.Length -1; i++)
+            { 
+                swapped = false ;
+
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // Swap
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+
+                        swapped = true;
+                    }
+
+                }
+
+                if(!swapped) 
+                    break ;
+            
+            }
+        }
+        #endregion
+        static void Main(string[] args)
+        {
+            #region Q1
+            /*The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm 
+            And implement the code of this optimized bubble sort algorithm */
+
+            // IF DURING FULL PASS OF THE ARRAY NO SWAPS ARE MADE THEN THE ARRAY IS ALREADY SORTED 
+
+            //int[] arr = { 8, 1, 2, 3, 4, 5, 6, 7 };
+            //OptimizeBubbleSort (arr);
+            //Console.WriteLine($"Sorted Array: {string .Join(", ",arr)}");
+
+
+
+
+
+            #endregion
+
+
+            #region Q2
+            // create a generic Range<T> class that represents a range of values from a minimum value to a maximum value. The range should support basic operations such as checking if a value is within the range and determining the length of the range.
+            #region REQ1
+            //Create a generic class named Range<T> where T represents the type of values.
+
+            //Range r = new Range();
+            #endregion
+
+            #region REQ2
+            //Implement a constructor that takes the minimum and maximum values to define the range.
+            Range<int> r01 = new Range<int>(4,9);
+
+            #endregion
+
+            #region REQ3
+            
+            //Implement a method IsInRange(T value) that returns true if the given  value is within the range, otherwise false.
+            
+            Console.WriteLine(r01.IsInRange(5));
+
+
+            #endregion
+
+            #region REQ4
+            //Implement a method Length() that returns the length of the range (the difference between the maximum and minimum values).
+            r01.Length();
+
+            #endregion
+
+            #region REQ5
+            //Note: You can assume that the type T used in the Range<T> class implements the IComparable<T> interface to allow for comparisons.
+
+            // THAT IS NOTE I DO IT
+            #endregion
+
+            #endregion
+        }
+    }
+}
---
+++ assigment_advanced_c#1/assigment_advanced_c#1/Range.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assigment_advanced_c_1
+{
+    internal class Range<T> where T : IComparable<T>
+    {
+        public T Min { get; }
+        public T Max { get; }
+        public Range(T min , T max)
+        {
+            Min = min;
+            Max = max;
+        }
+
+        public bool IsInRange(T Value)
+        {
+            return Value.CompareTo(Min) >=0 && Value.CompareTo(Max) <= 0; 
+        }
+
+        
+        public dynamic Length()
+        {
+            return (dynamic)Max - (dynamic)Min;
+        }
+    
+    }
+}
